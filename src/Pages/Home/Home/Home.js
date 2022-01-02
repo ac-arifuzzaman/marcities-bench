32f86b59f65e479ee68761864cc395c188542738
rased100 <rased100@gmail.com>
2022-01-02 11:53:10 +0600

    add by Rased

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 
 const Home = () => {
-    return (
-        <>
-          <Navigation></Navigation>
-          <h2>wellcome to home</h2>
-        </>
-    );
+  return (
+    <>
+      <Navigation></Navigation>
+      <h2>wellcome to home</h2>
+      <h3>Rased</h3>
+    </>
+  );
 };
 
 export default Home;
